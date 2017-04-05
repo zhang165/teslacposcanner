651e15b5f76a0050972f895e09e0a89200de7c32
MichaelZhang <zhang165@gmail.com>
2017-04-04 18:24:08 -0700

    Added scan functionality

--- src/main/java/tesla/cposcanner/scanner/WebScanner.java
+++ src/main/java/tesla/cposcanner/scanner/WebScanner.java
@@ -1,20 +1,34 @@
 package tesla.cposcanner.scanner;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class WebScanner {
 	private static final String URL_STRING = "https://www.tesla.com/preowned?model=ms";
-
-	public void scan() throws IOException {
-		Document doc = Jsoup.connect(URL_STRING).get();
-		Elements links = doc.select("a[href]");
-		System.out.println(links.toString());
+	private static final String API_STRING = "https://www.tesla.com/cpo_tool/ajax?exteriors=all&model=MODEL_S&priceRange=0%2C200000&city=null&state=null&country=US";
+	
+	public WebScanner(){
+		
 	}
-
+	
+	public void scan() throws IOException {
+		final URL url = new URL(API_STRING);
+		final URLConnection urlConnection = url.openConnection();
+		urlConnection.setDoOutput(true);
+		urlConnection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+		urlConnection.connect();
+		
+		String line;
+		try(final BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(),"UTF-8"));){
+			while((line = in.readLine()) != null){
+				System.out.println(line);
+			}
+		}	
+	}
 }
